0006ac68cd44b1c0d42d17c5e04e5a64178cea9e
Greyson <greyson.r.hamilton@gmail.com>
2020-10-28 21:04:47 -0400

    I think I understand

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 export default function App () {
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +33,15 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Link to='/'>The Movie List</Link>
+      <Link to='/'>The Movie</Link>
+      <Switch>
+        <Route exact path='/' render={props => 
+          <MovieList movies={MovieList} {...props}/>} />
+        <Route exact path='/movie/:id' render={props => 
+          <Movie {...props}/>} />
+        <Redirect to='/' />
+      </Switch>
     </div>
   );
 }
